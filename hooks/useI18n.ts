ffff5d3a73babcb426e86f5bec3b444da351b0d1
saad130013 <saad130013@gmail.com>
2026-01-01 04:09:56 +0300

    Fix import statement for useI18n hook

--- hooks/useI18n.ts
+++ hooks/useI18n.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { I18nContext } from '../context/I18nContext';
+import { useI18n } from "../hooks/useI18n";
 
 export const useI18n = () => {
   return useContext(I18nContext);
